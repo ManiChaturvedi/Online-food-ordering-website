2802454ad95b4f406a9a28fd714edbd5103f6d06
Mani Chaturvedi <145067644+ManiChaturvedi@users.noreply.github.com>
2024-07-02 14:28:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'; // Use 'react-dom' instead of 'react-dom/client' for React 17
 import './index.css';
 import App from './App';
-
 import reportWebVitals from './reportWebVitals';
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-      <App />
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
